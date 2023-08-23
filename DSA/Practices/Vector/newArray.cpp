d91da3dba5845a7a032dfcfea0359aab58e798ab
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-08-23 21:30:26 +0600

    push array in vector

--- DSA/Practices/Vector/newArray.cpp
+++ DSA/Practices/Vector/newArray.cpp
@@ -1,1 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    vector<int> v;
+    int i, n;
+
+    cin>>n;
+    int arr1[n];
+    int arr2[n];
+
+    for(i=0; i<n; i++)
+    {
+        cin>>arr1[i];
+
+    }
+
+    for(i=0; i<n; i++)
+    {
+        cin>>arr2[i];
+
+    }
+
+    for(i=0; i<n; i++)
+    {
+        v.push_back(arr2[i]);
+
+    }
+
+    for(i=0; i<n; i++)
+    {
+        v.push_back(arr1[i]);
+
+    }
+
+    for(int val: v)
+    {
+        cout<<val<<endl;
+
+    }
+
+    return 0;
+}
